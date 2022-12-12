626a91424b4cb025df6424a7df8c924ce015b9e3
dkovacec <dalibor.kovacec@zg.ht.hr>
2022-12-12 12:41:47 +0100

    Working on models - Updating users with custom details

--- src/main/java/com/brigths/Final/Countdown/Project/model/User.java
+++ src/main/java/com/brigths/Final/Countdown/Project/model/User.java
@@ -3,7 +3,6 @@ package com.brigths.Final.Countdown.Project.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.annotations.Table;
 
 import java.util.List;
 
@@ -27,13 +26,20 @@ public class User {
     @NotEmpty
     private String password;
 
+    private String country;
+    private String address;
+    private String address2;
+    private String postCode;
+    private String city;
+    private String phoneNumber;
+
+
     //one user can have more roles
     //one role can be given to many users
     @ManyToMany(cascade = CascadeType.MERGE, fetch  = FetchType.EAGER)
     @JoinTable(name = "user_role" , joinColumns = {@JoinColumn (name = "USER_ID", referencedColumnName = "ID")},
                 inverseJoinColumns = {@JoinColumn (name = "ROLE_ID", referencedColumnName = "ID")})
     private List<Role> roles;
-
 
     public User() {
 
@@ -94,5 +100,53 @@ public class User {
 
     public void setRoles(List<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
