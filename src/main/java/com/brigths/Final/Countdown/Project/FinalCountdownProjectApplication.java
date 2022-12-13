41a218499a94f55d3974a05fbfdcb4dbf9d25b6c
Ante Katalinić <ante.katalinic1@gmail.com>
2022-12-13 10:13:19 +0100

    Commit - added controller and methods for file upload, working get/post

--- src/main/java/com/brigths/Final/Countdown/Project/FinalCountdownProjectApplication.java
+++ src/main/java/com/brigths/Final/Countdown/Project/FinalCountdownProjectApplication.java
@@ -1,13 +1,24 @@
 package com.brigths.Final.Countdown.Project;
 
+import com.brigths.Final.Countdown.Project.service.FileStorageService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalCountdownProjectApplication {
+public class FinalCountdownProjectApplication implements CommandLineRunner {
+
+	@Resource
+	FileStorageService fileStorageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalCountdownProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//    storageService.deleteAll();
+		fileStorageService.init();
+	}
 }
---
+++ src/main/java/com/brigths/Final/Countdown/Project/controller/FilesController.java
@@ -0,0 +1,65 @@
+package com.brigths.Final.Countdown.Project.controller;
+
+import com.brigths.Final.Countdown.Project.model.FileInfo;
+import com.brigths.Final.Countdown.Project.model.ResponseMessage;
+import com.brigths.Final.Countdown.Project.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class FilesController {
+
+    FileStorageService fileStorageService;
+
+    @Autowired
+    public FilesController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            fileStorageService.save(file);
+
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<FileInfo>> getListFiles() {
+
+        List<FileInfo> fileInfos = fileStorageService.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder
+                    .fromMethodName(FilesController.class, "getFile", path.getFileName().toString()).build()
+                    .toString();
+
+            return  new FileInfo(filename, url);
+        }).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = fileStorageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
+
