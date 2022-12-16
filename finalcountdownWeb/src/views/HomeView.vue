84fea16f5ae3b2beb5d2c3d30a86be1a572b1651
Ante Katalinić <ante.katalinic1@gmail.com>
2022-12-16 11:06:49 +0100

    Commit - HomeView, added list of categories and router link with params. When mergeing to main router links should be added to router file.

--- finalcountdownWeb/src/views/HomeView.vue
+++ finalcountdownWeb/src/views/HomeView.vue
@@ -1,13 +1,16 @@
+<script>
+import Home from '../components/Home.vue';
+
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+
 <template>
 
-    
-    <h1>Welcome</h1>
-
-
+    <h1>BANNER GOES HERE</h1>
+    <Home></Home>
 
 </template>
-
-<style>
-
-
-</style>
