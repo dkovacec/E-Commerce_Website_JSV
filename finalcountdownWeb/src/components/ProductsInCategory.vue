067ea7fd4399e508fa38342aa2d50b5bf76905d4
dkovacec <dalibor.kovacec@zg.ht.hr>
2022-12-16 13:28:51 +0100

    Products in category niew

--- finalcountdownWeb/src/components/ProductsInCategory.vue
+++ finalcountdownWeb/src/components/ProductsInCategory.vue
@@ -7,20 +7,28 @@ export default {
 
     data() {
         return {
-
             productList: [],
-            totalProducts: 0
+            categoryList: [],
+            totalProducts: 0   
+
         };
-    },
-
-    created() {
-
     },
 
     methods: {
 
+        async getCategories() {
+            let url = '/api/categories'
+            try {
+                let response = await fetch(url);
+                this.categoryList = await response.json();
+                this.totalCategories = this.categoryList.length;
+            } catch (error) {
+                console.log("Error: ", error)
+            }
+        },
+
         async getProducts() {
-            //let urlGet = 'api/categories/1/products'
+            //let urlGet = '/api/categories/1/products'
             let urlGet = '/api/categories/' + this.$route.params.id + '/products'
             try {
 
@@ -31,28 +39,64 @@ export default {
                 console.log("Error: ", error)
 
             }
-        },
+        },   
+        
+        // getProducts() {
+        //     //let urlGet = '/api/categories/1/products'
+        //     let urlGet = '/api/categories/' + this.$route.params.id + '/products'
+        //     try {
+
+        //         let response = fetch(urlGet);
+        //         this.productList = response.json();
+        //         this.totalProducts = this.productList.length;
+        //     } catch (error) {
+        //         console.log("Error: ", error)
+
+        //     }
+        // },
+            
         created() {
+            this.getCategories();
             this.getProducts();
         },
+    
+
+        clicked() {           
+            this.created(),
+            this.$router.go('/api/categories/' + category.id + '/products')           
+            
+        }
 
     }
-
 }
 </script>
 
 <template>
-<h1>{{ this.$route.params.name }} {{ this.$route.params.id }}</h1>
+<h1>{{ this.$route.params.name }}</h1>
 
 <section class="productscategorized">
 
     <section class="categorylisted">
-  <p>  List of categories</p>
+  <h2 class="categories">Categories</h2>
+
+  <nav class="verticalMenu">
+
+        <ul>
+            <li v-for="category in categoryList" :key="category.id">
+                <router-link :to="{name:'ProductsInCategory', params: {id: category.id, name: category.name}}" @click="clicked">{{ category.name }} </router-link></li>
+        </ul>
+
+</nav>
+
     </section>
 
-    <section class="productCard" v-for="product in productList" :key="product.id">
+        <section class="productsamount">
+            Total number of products in category: {{ this.$route.params.name }} (id: {{ this.$route.params.id }}) <span> {{ totalProducts }}</span>
+        </section>
+
+            <section class="productCard" v-for="product in productList" :key="product.id">
         <h2 class="producttitle">{{ product.name }}</h2>
-        <p class="productimagebox"><img class="productimage" src={{product.imageName}}></p>
+        <p class="productimagebox"><img class="productimage" :src="(`../../${product.imageName}`)"></p>   <!-- :src="require(`@${product.imageName}`)"    :src=product.imageName-->
         <p class="productid">Product id: {{ product.id }}</p>
         <p class="summary"><b>Short description:</b><br><br> {{ product.summary }}</p>
         <p class="price"><b>Price:</b> €{{ product.price }}</p>
@@ -63,7 +107,10 @@ export default {
 
         </p>
 
+      
+
     </section>
+
 
 </section>
 
@@ -71,8 +118,9 @@ export default {
 
 <style>
 
+/*Products*/
 .productCard {
-    border: 1px solid black;
+    border: 1px solid rgba(113, 110, 110, 0.1);
     min-height: 380px;
     width: 73%;
     margin-left: auto;
@@ -82,7 +130,7 @@ export default {
 }
 
 .productimagebox{
-    border: 1px solid black;
+    /*border: 1px solid black;*/
     height: 285px;
     margin: 15px;
     float: left;
@@ -96,16 +144,6 @@ export default {
     background-color: rgb(216, 220, 225);
     padding: 15px;
     margin: 0;
-}
-
-.categorylisted {
-    border: 1px solid black;
-    min-height: 450px;
-    width: 22%;
-    float: left;
-    margin-right: auto;
-    /*margin-right: auto;*/
-    padding: 8px 8px 8px 8px;
 }
 
 .productscategorized {
@@ -165,6 +203,75 @@ export default {
 	border-radius: 5px;
     box-shadow: 0px 3px 5px #000;
     cursor: pointer;
+}
+.productsamount {
+    font-size: 0.7em;
+    text-align: right;
+    font-weight: bold;
+    padding: 10px;
+    color: gray;
+
+}
+
+/*CategoryMenu*/
+
+.categorylisted {
+    /*border: 1px solid black;*/
+    min-height: 450px;
+    width: 22%;
+    float: left;
+    margin-right: auto;
+    /*margin-right: auto;*/
+    padding: 5px;
+}
+
+.categories {
+    color: gray;
+    font-size: 1.3em;
+}
+
+.verticalMenu {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    box-shadow: 0px 3px 5px #000;
+    background-color: white;
+    /*display: inline-block;*/
+    /*overflow: hidden;*/
+
+    width: 98%;
+ 
+}
+.verticalMenu ul {
+    margin: 0;
+    padding: 0;
+
+}
+.verticalMenu li {
+    padding: 0;
+    margin-bottom: 5px;
+    text-align: left;
+    list-style: none;
+    border: 1px solid rgba(113, 110, 110, 0.4);
+}
+.verticalMenu a {
+    text-decoration: none;
+    color: black;
+    padding: 3px;
+    margin: 0;
+    display: block;
+    font-size: 1.4em;
+}
+.verticalMenu li a:hover {
+    cursor: pointer;
+    color: lightcyan;
+    background-color: black;
+}
+
+.verticalMenu li a:active {
+    color: black;
+    background-color: rgb(216, 220, 225);
 }
 
 
