860a3ed45a5b5a5af6c620a2100f9715f2052103
dkovacec <dalibor.kovacec@zg.ht.hr>
2022-12-16 11:25:17 +0100

    Products in category

--- finalcountdownWeb/src/views/HomeView.vue
+++ finalcountdownWeb/src/views/HomeView.vue
@@ -1,13 +1,15 @@
+<script>
+import Home from '../components/Home.vue';
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+
 <template>
 
-    
-    <h1>Welcome</h1>
-
-
+    <h1>BANNER GOES HERE</h1>
+    <Home></Home>
 
 </template>
-
-<style>
-
-
-</style>
