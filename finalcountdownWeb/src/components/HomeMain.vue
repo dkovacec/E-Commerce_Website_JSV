c5a22fe1808aec793eb4381e4a77136bd4de8d87
dkovacec <dalibor.kovacec@zg.ht.hr>
2022-12-20 09:27:02 +0100

    Changes for use administration

--- finalcountdownWeb/src/components/HomeMain.vue
+++ finalcountdownWeb/src/components/HomeMain.vue
@@ -1,74 +1,181 @@
 <script>
 export default {
-    mounted: function () {
-        this.created()
+  mounted: function () {
+    this.created()
+  },
+  data() {
+    return {
+      categoryList: [],
+      productList: []
+    };
+  },
+  methods: {
+    async getCategories() {
+      let url = 'api/categories'
+      try {
+        let response = await fetch(url);
+        this.categoryList = await response.json();
+        this.totalCategories = this.categoryList.length;
+      } catch (error) {
+        console.log("Error: ", error)
+      }
     },
-    data() {
-        return {
-            categoryList: [],
-        };
+    created() {
+      this.getCategories();
+      this.getProducts();
     },
-    methods: {
-        async getCategories() {
-            let url = 'api/categories'
-            try {
-                let response = await fetch(url);
-                this.categoryList = await response.json();
-                this.totalCategories = this.categoryList.length;
-            } catch (error) {
-                console.log("Error: ", error)
-            }
-        },
-        created() {
-            this.getCategories();
-        },
-    }
+    async getProducts() {
+      let url = 'api/products'
+      try {
+        let response = await fetch(url);
+        this.productList = await response.json();
+        this.totalProducts = this.productList.length;
+      } catch (error) {
+        console.log("Error: ", error)
+      }
+    },
+  }
 }
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "25px";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 </script>
 
 <template>
 
-<nav class="ctgMenu">
-
+  <nav id="navbar">
     <ul>
-        <li v-for="category in categoryList" :key="category.id" >
-            <router-link :to="{name:'ProductsInCategory', params: {id: category.id, name: category.name}}">{{ category.name }} </router-link></li>
+      <li v-for="category in categoryList" :key="category.id">
+        <router-link :to="{ name: 'ProductsInCategory', params: { id: category.id, name: category.name } }">{{
+            category.name
+        }} </router-link>
+      </li>
     </ul>
+  </nav>
 
-</nav>
+
+
+  <div class="card" v-for="product in productList" :key="product.id">
+    <router-link :to="{ name: 'ProductDetail', params: { productid: product.id } }">
+      <img class="imageCard" :src="product.imageName">
+      <p class="title">{{ product.name }}</p>
+      <p class="price">€{{ product.price }}</p>
+      <!-- <p class="summary">{{product.summary}}</p> -->
+    </router-link>
+  </div>
+
+
+
 </template>
 
 
 <style scoped>
-.ctgMenu {
-    position: absolute;
-    list-style: none;
-    margin:auto;
-    text-align: center;
-    box-shadow: 0px 3px 5px #000;
-    background-color: white;
-    /*display: inline-block;*/
-    /*overflow: hidden;*/
-    position: fixed;
-    width: 75%;
+#ctgMenu {
+  position: fixed;
+  display: block;
+  margin-top: -210px;
+  margin-left: 0;
+  text-align: center;
+  box-shadow: 0px 3px 5px #000;
+  background-color: white;
+  /*display: inline-block;*/
+  /*overflow: hidden;*/
+  position: fixed;
+  width: 75%;
 }
-.ctgMenu li {
-    display: inline;
-    padding-left: 5%;
-    padding-right: 5%;
-    text-align: center;
-    /* border: 1px solid black; */
+#ctgMenu li {
+  display: inline;
+  padding-left: 5%;
+  padding-right: 5%;
+  text-align: center;
+  /* border: 1px solid black; */
 }
-.ctgMenu a {
-    text-decoration: none;
-    color: black;
-    padding: 10px 20px;
-    margin: 0;
-    display: inline-block;
+#ctgMenu a {
+  text-decoration: none;
+  color: black;
+  padding: 10px 20px;
+  margin: 0;
+  display: inline-block;
 }
-.ctgMenu li a:hover {
-    cursor: pointer;
-    color: lightcyan;
-    background-color: black;
+#ctgMenu li a:hover {
+  cursor: pointer;
+  color: lightcyan;
+  background-color: black;
+}
+#navbar {
+  background-color: #333;
+  position: fixed;
+  top: 25px;
+  margin-left: 0px;
+  width: 75%;
+  text-align: center;
+  box-shadow: 0px 3px 5px #000;
+  display: block;
+  transition: top 1s;
+}
+#navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 15px;
+  text-decoration: none;
+  font-size: 17px;
+}
+#navbar li a:hover {
+  cursor: pointer;
+  color: lightcyan;
+  background-color: black;
+}
+#navbar li {
+  float:left;
+  display: inline;
+  padding-left: 5%;
+  padding-right: 5%;
+  text-align: center;
+  /* border: 1px solid black; */
+}
+#navbar a {
+  text-decoration: none;
+  color: white;
+  padding: 10px 20px;
+  margin: 0;
+  display: inline-block;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  min-width: 300px;
+  max-width: 400px;
+  min-height: 350px;
+  margin-top: 8%;
+  text-align: center;
+  float:left;
+  margin-left: 2.8rem;
+}
+.card .title {
+  font-weight: bold;
+}
+.card:hover {
+  opacity: 1;
+  cursor: pointer;
+}
+.price {
+  color: grey;
+  font-size: 22px;
+}
+.card .imageCard {
+  width: 300px;
+  height: 300px;
+}
+.card a {
+text-decoration: none;
+color: black;
 }
 </style>
--- finalcountdownWeb/src/views/Home.vue
+++ finalcountdownWeb/src/views/Home.vue
@@ -18,15 +18,15 @@ export default defineComponent({
 <h1>BANNER GOES HERE</h1>
 <HomeMain></HomeMain>
 
-  <p>This application provides a simple introduction for enabling user authentication for a VueJS web application with a Spring Boot
+  <!-- <p>This application provides a simple introduction for enabling user authentication for a VueJS web application with a Spring Boot
     backend.</p>
 
-  <p>This example uses <abbr title="JSON Web Token">jwt</abbr> for storing and transporting login information.</p>
+  <p>This example uses <abbr title="JSON Web Token">jwt</abbr> for storing and transporting login information.</p> -->
 
-  <div class="authenticated" v-if="authStore.isAuthenticated">
+  <!-- <div class="authenticated" v-if="authStore.isAuthenticated">
     You are only seeing this message when you are logged in!
     Great you just logged inn
-  </div>
+  </div> -->
 
 
 
